17bc062558b3c2f4fafa15647e82ea8765e7e46d
Steven McNichol <smcnichol@outlook.com>
2026-04-28 22:47:01 +1000

    chore: fix signing keystore path + redirect build dirs for correct permissions

---
+++ run_gradle_release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export JAVA_HOME='/Applications/Android Studio.app/Contents/jbr/Contents/Home'
+export PATH="$JAVA_HOME/bin:/usr/local/bin:/opt/homebrew/bin:$PATH"
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+cd "$HOME/Documents/mcnichol-invoices/android"
+./gradlew bundleRelease > /tmp/gradle_build.log 2>&1
+echo "EXIT:$?" >> /tmp/gradle_build.log
